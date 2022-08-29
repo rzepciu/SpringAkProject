0d06cf159ae12268873874c7495f972bbbf47d39
Rzepecki <andrzej.rzepecki@accenture.com>
2022-08-29 07:56:49 +0200

    init

---
+++ src/test/java/com/example/spring_std_project/SpringStdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_std_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStdProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
