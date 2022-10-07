5aa6332f50dc29aa73e557320b0c54c8e0f6ceb6
rzepciu <arzepecki@op.pl>
2022-10-07 20:15:54 +0200

    mod 3 final lesson (#3)
    Co-authored-by: Rzepecki <andrzej.rzepecki@accenture.com>

--- src/main/java/com/example/spring_std_project/SpringStdProjectApplication.java
+++ src/main/java/com/example/spring_std_project/SpringStdProjectApplication.java
@@ -3,11 +3,11 @@ package com.example.spring_std_project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SpringStdProjectApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringStdProjectApplication.class, args);
-    }
+//    public static void main(String[] args) {
+//        SpringApplication.run(SpringStdProjectApplication.class, args);
+//    }
 
 }
