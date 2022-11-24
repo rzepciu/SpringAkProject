b887f760fb54bb84f9275b9e7148f07a1241b41a
andrzej.rzepecki <andrzej.rzepecki@accenture.com>
2022-11-24 07:40:47 +0100

    mod04_thymeleaf iteracja po kolekcji

--- src/main/java/com/github/rzepciu/mod4/thymeleaf/controler/CarController.java
+++ src/main/java/com/github/rzepciu/mod4/thymeleaf/controler/CarController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class CarController {
 
@@ -12,8 +15,13 @@ public class CarController {
     public String getCar(Model model){
 
         Car car =  new Car("Bmw", "I8");
+        Car car2 =  new Car("Audi", "A4");
+        Car car3 =  new Car("Opel", "Vectra");
+        Car car4 =  new Car("Opel", "Astra");
+        List<Car> cars = Arrays.asList(car,car2,car3,car4);
         model.addAttribute("name","andrzej");
         model.addAttribute("car",car);
+        model.addAttribute("cars",cars);
 
         return "car";
     }
